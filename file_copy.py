24a170636554b2f50425f9fb303e274b12b402e5
Vincent Healy <vchealy@gmail.com>
2020-08-15 06:34:31 +0100

    Update file_copy.py

--- file_copy.py
+++ file_copy.py
@@ -3,12 +3,13 @@ import shutil, os
 def list_folders_files(path):
     '''
     Creat a list of folders and files.
-    Slice out some folders
+    Slice, as a form of filter, the folders of interest
     '''
+   file_path = 'V:\Python\pyBites'
     path = path
     folders = os.listdir(path)
     folder_list = []
-    for folder in folders[3:-18]: #Slicing the folders from the list
+    for folder in folders[3:-18]: # Slicing the folders from the list that I wanted
         folder_list.append(str(folder))
     return folder_list
 
@@ -16,13 +17,13 @@ def list_folders_files(path):
 def filecopy(*args):
     files = [*args]
     print(files)
-    folders = list_folders_files('V:\Python\pyBites')
+    folders = list_folders_files(file_path)
     print(folders)
     for folder in folders:
         try:
             for file in files:
-                print(f'V:\Python\pyBites\{folder}')
-                shutil.copy(file, f'V:\Python\pyBites\{folder}')
+                print(f'{file_path}\{folder}')
+                shutil.copy(file, f'{file_path}{folder}')
         except SameFileError as e:
             print(f'File already Exists: {e}')
         except:
@@ -32,4 +33,4 @@ def filecopy(*args):
 
 # Tests
 # print(list_folders_files('V:\Python\pyBites'))
-filecopy('decorators.py','key_value_swapper.py', 'in_stock.py')
+# filecopy('decorators.py','key_value_swapper.py', 'in_stock.py')
