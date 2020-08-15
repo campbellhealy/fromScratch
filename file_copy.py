b5f540e02833eeecf2841f2aedce09702c2ba05e
Vincent Healy <vchealy@gmail.com>
2020-08-15 06:50:39 +0100

    Update file_copy.py

--- file_copy.py
+++ file_copy.py
@@ -1,15 +1,19 @@
-import shutil, os
+import shutil
+import os
+
+
+file_path = 'V:\\Python\\pyBites'
+
 
 def list_folders_files(path):
     '''
     Creat a list of folders and files.
     Slice, as a form of filter, the folders of interest
     '''
-   file_path = 'V:\Python\pyBites'
     path = path
     folders = os.listdir(path)
     folder_list = []
-    for folder in folders[3:-18]: # Slicing the folders from the list that I wanted
+    for folder in folders[3:-18]:  # Slicing the folders from the list that I wanted
         folder_list.append(str(folder))
     return folder_list
 
@@ -22,7 +26,7 @@ def filecopy(*args):
     for folder in folders:
         try:
             for file in files:
-                print(f'{file_path}\{folder}')
+                print(f'{file_path}{folder}')
                 shutil.copy(file, f'{file_path}{folder}')
         except SameFileError as e:
             print(f'File already Exists: {e}')
@@ -31,6 +35,7 @@ def filecopy(*args):
 
     print('Done')
 
+
 # Tests
 # print(list_folders_files('V:\Python\pyBites'))
-# filecopy('decorators.py','key_value_swapper.py', 'in_stock.py')
+filecopy('decorators.py', 'key_value_swapper.py', 'in_stock.py')
