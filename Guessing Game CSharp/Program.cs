bb3c0ac1ea96752ee0e55f55d30e72125a5beb72
Vincent Healy <vchealy@gmail.com>
2021-01-07 22:24:20 +0000

    Guessing Game in C#
    Console App is written in C#
    I found a bust version of this online to give me an idea of what it was doing. I looked it over and rewrote it.
    The initial writing was to get the basic functionality working.
    Then add in some control over what was guessed.
    It will only accept numbers between 1 to 100, no other characters
    Only allows 5 guesses, error guesses did not increment the guess count
    Clears the console at the start of the game and just before the end, if the game is won.

---
+++ Guessing Game CSharp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using static System.Console;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int countGuess = 1;             // Counter to limit the number of turns
+            int guess = 0;                  // Initiate variable
+            string welcome = "Guess a number between 1 and 100: ";
+            int num = rand.Next(1, 100);    //Set the number to be guessed
+            Clear();                        //Clears the console
+            Write(welcome);
+
+
+            while (guess != num)
+            {
+                try
+                {
+                    guess = Convert.ToInt32(ReadLine());
+                    if (guess > 100 || guess < 0)  //This ensures the guess is in the correct range
+                    {
+                        Write("Guess must be between 1 and 100: ");
+                        countGuess --; // This resets the counter by -1
+                    }
+                    if (guess > num && guess < 101)
+                    {
+                        if (countGuess>4)     // Check if 5 guesses have been used and last guess wrong 
+                        {
+                            WriteLine();
+                            WriteLine("You Lose all 5 turns have been used.");
+                            WriteLine($"The number you wanted was {num}");
+                            WriteLine();
+                            Environment.Exit(0);
+                        }
+                        WriteLine($" That was turn number {countGuess} of 5");
+                        Write($"Too High, Guess a value lower than {guess}: ");
+                    }
+                    else if (guess < num && guess > 0)
+                    {
+                        if (countGuess>4)   // Check if 5 guesses have been used and last guess wrong
+                        {
+                            Clear();
+                            WriteLine();
+                            WriteLine("You Lose all 5 turns have been used.");
+                            WriteLine($"The number you wanted was {num}");
+                            WriteLine();
+                            Environment.Exit(0);
+                        }
+                        WriteLine($" That was turn number {countGuess} of 5");
+                        Write($"Too Low, Guess a value higher than {guess}: ");
+                    }
+                }
+                catch
+                {
+                    Write("Guess must be a number between 1 and 100: "); 
+                    countGuess--;
+                }
+                countGuess++;
+            }
+            countGuess--;
+            WriteLine("Congrats, it took you " + countGuess + " tries");
+            Environment.Exit(0);
+        }
+    }
+}
