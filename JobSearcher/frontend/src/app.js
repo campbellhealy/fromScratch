07e55c4630c6c00533aca80ea1cd19b1468c8e5c
Vincent Healy <vchealy@gmail.com>
2023-04-24 20:28:22 +0100

    Delete JobSearcher directory

--- JobSearcher/frontend/src/app.js
+++
@@ -1,6 +0,0 @@
-// Main
-import { JobSearch } from './JobSearch';
-
-const jobSearch = new JobSearch('#search-form', '.result-container', '.loading-element');
-jobSearch.setCountryCode();
-jobSearch.configureFormListener();
